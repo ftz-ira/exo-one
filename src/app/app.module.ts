30a91441d80c5552b4105156e614c1419899def4
ftz <frantziraci@gmail.com>
2019-06-26 09:14:53 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostListComponentComponent } from './post-list-component/post-list-component.component';
+import { PostListItemComponent } from './post-list-item/post-list-item.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+ 
+ declarations: [
+    AppComponent,
+    PostListComponentComponent,
+    PostListItemComponent
   ],
   imports: [
     BrowserModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
